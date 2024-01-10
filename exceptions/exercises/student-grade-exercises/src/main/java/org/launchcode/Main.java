74a942aa9c121c9fe43e95043aed6b8f73be8e9c
Joshua Buehrer <louisproductions179@gmail.com>
2024-01-09 18:46:32 -0600

    EXERCISES: EXCEPTIONS

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -15,13 +15,37 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        try {
+            if (y == 0) {
+                throw new ArithmeticException("Cannot divide by zero");
+            }
+
+            return x / y;
+        } catch (ArithmeticException e) {
+
+            System.out.println("Error in Divide(): " + e.getMessage());
+
+            throw e;
+        }
     }
+
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        try {
+            if (fileName.isEmpty()) {
+                throw new RuntimeException("Files can't be empty! -1 to gryffindor");
+            }
+            if (fileName.endsWith(".java")){
+                return 1;
+            } else {
+                return 0;
+            }
+        } catch (RuntimeException e){
+            System.out.println("Error in Runtime Exception");
+            throw e;
+        }
     }
+
 }
